6853627663c6936204357305a3b7ea604dc7e6bf
Yashggoswami <yashgoswami2000@gmail.com>
2021-09-09 15:51:06 +0530

    hbs file linking- project work

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,1 +1,6 @@
 // user authentication i.e. login, register logout work will be done here
+login = (err,req,res,next)=>{
+res.render("login")
+}
+
+module.exports = login
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,8 @@
-const express = require('express'),
-    app = express()
-
+const app = require('express').Router()
+const user = require('../controllers/user')
 // <------------- all routing work will be done here ----------->
-app.get('/',(req,res) => res.send("inside router file"))
-
+app.get('/',(req,res) => {res.render('dashboard')})
+// app.get('/login',user.login)
 
 
 
