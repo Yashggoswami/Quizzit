53795daca2f9fa249b90e5213164c8674257ae47
Yashggoswami <yashgoswami2000@gmail.com>
2021-09-10 11:05:59 +0530

    register.css issue resolve - project working

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express'),
+    app = express(),
+    hbs = require('hbs'),
+    error_handler = require('./src/services/error_handler')
+
+
+ app.use(express.static("./src/public"))
+//app.use(express.static(__dirname + 'src/public'))
+// directing to routes directory for routing purpose
+app.use('/',require('./src/routes'))
+
+app.set('views','src/views')
+app.set('view engine','hbs')
+hbs.registerPartials('src/views/partials')
+app.use(error_handler)
+
+app.listen(8080,(req,res) => {
+    console.log("server listening to http://localhost:8080")
+})
--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-const express = require('express'),
-    app = express(),
-    hbs = require('hbs'),
-    error_handler = require('./services/error_handler')
+// const express = require('express'),
+//     app = express(),
+//     hbs = require('hbs'),
+//     error_handler = require('./services/error_handler')
 
 
- app.use(express.static("src/public"))
-//app.use(express.static(__dirname + 'src/public'))
-// directing to routes directory for routing purpose
-app.use('/',require('./routes'))
+//  app.use(express.static("src/public"))
+// //app.use(express.static(__dirname + 'src/public'))
+// // directing to routes directory for routing purpose
+// app.use('/',require('./routes'))
 
-app.set('views','src/views')
-app.set('view engine','hbs')
-hbs.registerPartials('src/views/partials')
-app.use(error_handler)
+// app.set('views','src/views')
+// app.set('view engine','hbs')
+// hbs.registerPartials('src/views/partials')
+// app.use(error_handler)
 
-app.listen(8080,(req,res) => {
-    console.log("server listening to http://localhost:8080")
-})
+// app.listen(8080,(req,res) => {
+//     console.log("server listening to http://localhost:8080")
+// })
