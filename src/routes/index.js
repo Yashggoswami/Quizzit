f3a70dec47be924f2aa41fbd54e19bd38473aac0
Yashggoswami <yashgoswami2000@gmail.com>
2021-09-10 21:10:48 +0530

    authentication and login database connectivity and functionality added - project working

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,13 @@ const user = require('../controllers/user')
 // <------------- all routing work will be done here ----------->
 app.get('/login',user.login)
 // app.post('/authenticate/:username/:password',user.authenticate)
-app.post('/authenticate',user.authenticate)
+// app.post('/authenticate',user.authenticate)
 app.get('/signup',user.register)
-app.post('/createAccount',user.createAccount)
+// app.post('/createAccount',user.createAccount)
 app.get('/',(req,res) => {res.render('dashboard')})
 
+app.post('/authenticate', user.authenticateSchema, user.authenticate);
+app.post('/createAccount', user.registerSchema, user.register);
 
 
 
