e5d0ea68e727a926a422db73cdca3882e76bbebc
Yashggoswami <yashgoswami2000@gmail.com>
2021-09-10 19:54:26 +0530

    input form activiation for login and registeration - project working

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express'),
     app = express(),
     hbs = require('hbs'),
+    bodyparser = require('body-parser')
     error_handler = require('./src/services/error_handler')
 
-
+app.use(bodyparser.urlencoded({extended:true}))
  app.use(express.static("./src/public"))
 //app.use(express.static(__dirname + 'src/public'))
 // directing to routes directory for routing purpose
--- src/public/js/register.js
+++ src/public/js/register.js
@@ -1,5 +1,3 @@
 $(document).ready(function(){
-    console.log("comming");
-    $('#birth-date').mask('00/00/0000');
-    $('#phone-number').mask('0000-0000');
+   
    })
